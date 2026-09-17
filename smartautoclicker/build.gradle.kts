667546a6ea8289a4fbc267055e575278b274d0c7
slobys <slobys@users.noreply.github.com>
2026-09-17 16:12:16 +0800

    Redesign collapsed menu launcher icon

--- smartautoclicker/build.gradle.kts
+++ smartautoclicker/build.gradle.kts
@@ -57,8 +57,8 @@ android {
     defaultConfig {
         applicationId = getExtraActualApplicationId()
 
-        versionCode = 109
-        versionName = "4.0.5.9"
+        versionCode = 110
+        versionName = "4.0.5.10"
     }
 
     if (project.isBuildForVariant(KlickrFlavour.F_DROID, KlickrBuildType.DEBUG)) {
