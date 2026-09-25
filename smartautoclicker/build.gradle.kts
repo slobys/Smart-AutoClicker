f1c9409231d5ab0130b1eb03b0f3ad95b1064aee
slobys <slobys@users.noreply.github.com>
2026-09-25 11:19:06 +0800

    Release v4.0.5.23

--- smartautoclicker/build.gradle.kts
+++ smartautoclicker/build.gradle.kts
@@ -57,8 +57,8 @@ android {
     defaultConfig {
         applicationId = getExtraActualApplicationId()
 
-        versionCode = 122
-        versionName = "4.0.5.22"
+        versionCode = 123
+        versionName = "4.0.5.23"
     }
 
     if (project.isBuildForVariant(KlickrFlavour.F_DROID, KlickrBuildType.DEBUG)) {
